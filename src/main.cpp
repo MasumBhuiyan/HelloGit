cb31e2d1036d0c3be27702db1ae0714ae05d5057
Bhuiyan007 <119737872+Bhuiyan007@users.noreply.github.com>
2022-12-03 18:59:18 +0600

    [HelloGit] Added an add function to add two integers
    [Issue] N/A
    [Request] N/A
    [Occurence version] N/A
    
    [Problem] Require an add function to add two integers
    [Cause & Measure]
        Cause: Add function was not implemented
        Measure: Implemented an add function
    [Checking Method] Dev
    
    [Team] GitTeam
    [Developer] Md. Masum Bhuiyan [Bhuiyan007]
    [Solution Company] Bhuiyans.io
    [Change type] Patch
    
    Signed-off-by: Masum Bhuiyan <masumishere@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 using namespace std;
 
+int add(int a, int b) {
+    return a + b;   
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a + b << endl;
+    cout << add(a, b) << endl;
     return 0;
 }
