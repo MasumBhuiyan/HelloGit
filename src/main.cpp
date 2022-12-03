203dfec614234693e7fccf809b97b4fa4e21e8bb
Bhuiyan007 <119737872+Bhuiyan007@users.noreply.github.com>
2022-12-03 19:18:28 +0600

    [HelloGit] Added an subtract, multiply, and division function
    [Issue] N/A
    [Request] N/A
    [Occurence version] N/A
    
    [Problem] Require an add function to add two integers
    [Cause & Measure]
        Cause: Add function was not implemented
        Measure: Implemented an add function
    [Checking Method] Dev
    
    [Team] GitTeam
    [Developer] Md. Masum Bhuiyan [Bhuiyan007]
    [Solution Company] Bhuiyans.io
    [Change type] Patch
    
    Signed-off-by: Masum Bhuiyan <masumishere@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@ int add(int a, int b) {
     return a + b;   
 }
 
+int subtract(int a, int b) {
+    return a - b;   
+}
+
+int multiply(int a, int b) {
+    return a * b;   
+}
+
+int divide(int a, int b) {
+    return a / b;   
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
